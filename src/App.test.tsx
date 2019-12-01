9ab133fc44935944e5b9f6a95ec1c775f0cb24de
sampsonoliver <sampsonjoliver@outlook.com>
2019-12-01 11:04:57 +1100

    Update App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const mockAuthProvider = {};
+const mockContext = React.createContext(mockAuthProvider);
+jest.mock('./contexts/Auth0Provider', () => {
+  return {
+    Auth0Provider: ({ children }: any) => (
+      <mockContext.Provider value={mockAuthProvider}>
+        {children}
+      </mockContext.Provider>
+    ),
+    useAuth0: () => mockAuthProvider,
+  };
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
