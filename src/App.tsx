b1c896d5c839661403b2a06238ae8aee32281fc6
sampsonoliver <sampsonjoliver@outlook.com>
2019-12-10 12:22:15 +1100

    Add persistent bg color

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { ConfigProvider } from './contexts/ConfigProvider';
 import { BottomNav } from './components/BottomNav';
 import { Router } from './components/Router';
 import { BrowserRouter } from 'react-router-dom';
+import { Box } from '@material-ui/core';
+import { colours } from './theme';
 
 const ConfiguredApp: React.FC = () => {
   return (
@@ -18,6 +20,13 @@ const App: React.FC = () => {
   return (
     <Auth0Provider onRedirectCallback={() => null}>
       <BrowserRouter>
+        <Box
+          bgcolor={colours.navy600}
+          height="100vh"
+          width="100vw"
+          position="fixed"
+          zIndex={-100}
+        />
         <Router />
         <BottomNav />
       </BrowserRouter>
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+export const colours = {
+  navy800: '#050035',
+  navy700: '#080248',
+  navy600: '#201B5A',
+  indigo700: '#261E7D',
+  indigo200: '#C9C7DE',
+  lgrey800: '#CAD6EB',
+  lgrey400: '#EDF1F8',
+  lgrey100: '#F9FBFD',
+  green700: '#02E088',
+  pink700: '#FF1675',
+  orange700: '#FD8A15',
+};
