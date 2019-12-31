36adb808a8da6ed5b4b54ac73379a95382c05a58
Sampson Oliver <sampsonjoliver@outlook.com>
2019-12-31 14:53:03 +1100

    Use react router hooks instead of Route renderprop

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import { Dashboard } from '../screens/Dashboard';
 import { Downloads } from '../screens/Downloads';
 import { Settings } from '../screens/Settings';
@@ -10,46 +10,52 @@ import { colours } from '../theme';
 
 import { Fade, Box } from '@material-ui/core';
 
+const Background = () => (
+  <Box
+    bgcolor={colours.navy600}
+    height="100vh"
+    width="100vw"
+    position="fixed"
+    zIndex={-100}
+  />
+);
+
 export const Router: React.FC = () => {
+  const dashboardMatch = useRouteMatch({
+    path: '/',
+    exact: true,
+  });
+  const downloadsMatch = useRouteMatch({
+    path: '/downloads',
+  });
+  const settingsMatch = useRouteMatch({
+    path: '/settings',
+  });
+
   return (
     <Screen height="100vh" display="flex" flexDirection="column">
-      <Box
-          bgcolor={colours.navy600}
-          height="100vh"
-          width="100vw"
-          position="fixed"
-          zIndex={-100}
-        />
-        <AppBar />
-      <Route path="/downloads">
-        {({ match }) => (
-          <Fade in={match != null} unmountOnExit>
-            <Box position="absolute" width="100vw" mt="64px" pb="56px">
-              <Downloads />
-            </Box>
-          </Fade>
-        )}
-      </Route>
-      <Route path="/settings">
-        {({ match }) => (
-          <Fade in={match != null} unmountOnExit>
-            <Box position="absolute" width="100vw" mt="64px" pb="56px">
-              <Settings />
-            </Box>
-          </Fade>
-        )}
-      </Route>
-      <Route path="/" exact>
-        {({ match }) => (
-          <Fade in={match != null} unmountOnExit>
-            <Box position="absolute" width="100vw" mt="64px" pb="56px">
-              <Dashboard />
-            </Box>
-          </Fade>
-        )}
-      </Route>
-      <BottomNav />
+      <Background />
+      <AppBar />
 
+      <Fade in={downloadsMatch != null} unmountOnExit>
+        <Box position="absolute" width="100vw" mt="64px" pb="56px">
+          <Downloads />
+        </Box>
+      </Fade>
+
+      <Fade in={settingsMatch != null} unmountOnExit>
+        <Box position="absolute" width="100vw" mt="64px" pb="56px">
+          <Settings />
+        </Box>
+      </Fade>
+
+      <Fade in={dashboardMatch != null} unmountOnExit>
+        <Box position="absolute" width="100vw" mt="64px" pb="56px">
+          <Dashboard />
+        </Box>
+      </Fade>
+
+      <BottomNav />
     </Screen>
   );
 };
