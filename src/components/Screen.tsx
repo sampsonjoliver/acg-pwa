3130be9c634d572a1f912ae2fa30415c3add7550
Sampson Oliver <sampsonjoliver@outlook.com>
2020-01-21 21:49:27 +1100

    Make Screen set sane app defaults

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import Box, { BoxProps } from '@material-ui/core/Box';
 
 export const Screen: React.FC<BoxProps> = ({ children, ...props }) => {
-  return <Box {...props}>{children}</Box>;
+  return (
+    <Box height="100vh" display="flex" flexDirection="column" {...props}>
+      {children}
+    </Box>
+  );
 };
